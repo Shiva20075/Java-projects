a84b219f73b14ac9818f7136f15e542451aefd81
shiva20075 <palleshiva2007@gmail.com>
2025-07-09 18:12:28 +0530

    code optimised

--- src/Song/SongRunner.java
+++ src/Song/SongRunner.java
@@ -4,19 +4,20 @@ public class SongRunner {
 
     public static void main (String[] args){
 
-        Songs songRunner = new Songs("HONEY", "sai", 4.5);
+        Songs songRunner = new Songs("JOHN", "sai", 4.5);
 
         songRunner.setSongName("SHAPE OF U");
         songRunner.setArtist("SHIVA");
         songRunner.setSongDuration(3.4);
      //   songRunner.songInfo();
 
-        Songs songRunner2 = new Songs("HONEY2", "sai2", 4.5);
+        Songs songRunner2 = new Songs("SENA", "sai2", 4.5);
 
         Playlist playList = new Playlist();
 
         playList.addSong(songRunner);
         playList.addSong(songRunner2);
+        playList.showSong();
         playList.showSong();
     }
 }
